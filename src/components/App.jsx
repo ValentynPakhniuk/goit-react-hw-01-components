0d32c1e30fc3ba9d1c7c1c073a48db35784a88bf
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-03-27 02:06:48 +0300

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Profile } from './Profile';
+import user from './Profile/user.json';
+import { Statistics } from './Statistics';
+import data from './Statistics/data.json';
+import { FriendList } from './FriendList';
+import friends from './FriendList/friends.json';
+import { TransactionHistory } from './TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
+import { Container } from './container.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" data={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
   );
 };
